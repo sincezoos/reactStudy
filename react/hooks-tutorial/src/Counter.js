21758872e041c7e92c858c497068632d9da98939
sincezoos <sincezoos@naver.comm>
2023-03-05 23:25:26 +0900

    init commit

--- react/hooks-tutorial/src/Counter.js
+++ react/hooks-tutorial/src/Counter.js
@@ -6,7 +6,7 @@ const Counter = () => {
     return (
         <div>
             <p>
-                현재 카운터 값은 <b>{value}</b>입니다.
+                현재 카운터 값은 <b>{value}</b> 입니다.
             </p>
             <button onClick={() => setValue(value + 1)}>+1</button>
             <button onClick={() => setValue(value - 1)}>-1</button>
